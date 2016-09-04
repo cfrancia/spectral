d526284d41b8fea270585455ecfb4e68217e71d4
cfrancia <cfrancia@users.noreply.github.com>
2016-09-04 16:56:09 +1000

    can assert on vec length and map to an internal field of a struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,36 @@ use std::cmp::PartialEq;
 use std::fmt::Display;
 
 #[derive(Debug)]
-pub struct Spec<'s, S: 's + Display> {
+pub struct Spec<'s, S: 's> {
     pub subject: &'s S,
 }
 
-impl<'s, S> Spec<'s, S> where S: Display {
+impl<'s, S> Spec<'s, S> {
     pub fn assert_that(subject: &'s S) -> Spec<'s, S> {
         Spec { subject: subject }
     }
 }
 
 impl<'s, S> Spec<'s, S> where S: Display + PartialEq {
-    pub fn is_equal_to(&self, actual: &S) {
-        if !self.subject.eq(actual) {
-            panic!(format!("expected <{}> but was <{}>", actual, self.subject));
+    pub fn is_equal_to(&self, expected: &S) {
+        if !self.subject.eq(expected) {
+            panic!(format!("expected <{}> but was <{}>", expected, self.subject));
         }
     }
+}
+
+impl<'s, T> Spec<'s, Vec<T>> {
+    pub fn has_length(&self, expected: usize) {
+        let length = self.subject.len();
+        if length != expected {
+            panic!(format!("expected vec with length of <{}> but was <{}>", expected, length));
+        }
+    }
+}
+
+impl<'s, S> Spec<'s, S> {
+    pub fn map<F, T>(self, mapping_function: F) -> Spec<'s, T>
+        where F: Fn(&'s S) -> &'s T {
+            Spec { subject: mapping_function(self.subject) }
+        }
 }
--- tests/spectral.rs
+++ tests/spectral.rs
@@ -12,3 +12,26 @@ fn should_not_panic_on_equal_subjects() {
 fn should_panic_on_unequal_subjects() {
     Spec::assert_that(&1).is_equal_to(&2);
 }
+
+#[test]
+fn should_not_panic_if_vec_length_matches_expected() {
+    let test_vec = vec![1,2,3];
+    Spec::assert_that(&test_vec).has_length(3);
+}
+
+#[test]
+#[should_panic]
+fn should_panic_if_vec_length_does_not_match_expected() {
+    let test_vec = vec![1,2,3];
+    Spec::assert_that(&test_vec).has_length(1);
+}
+
+#[test]
+fn should_be_able_to_map_to_inner_field_of_struct_when_matching() {
+    let test_struct = TestStruct { value: 5 };
+    Spec::assert_that(&test_struct).map(|val| &val.value).is_equal_to(&5);
+}
+
+struct TestStruct {
+    pub value: u8,
+}
