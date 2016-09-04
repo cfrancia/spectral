710ec9b528918b2d34dccf745e00e336dc61d762
cfrancia <cfrancia@users.noreply.github.com>
2016-09-04 18:32:42 +1000

    Can now match on option variants

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,37 @@ impl<'s, T> Spec<'s, Vec<T>> {
     }
 }
 
+impl<'s, T> Spec<'s, Option<T>> where T: Display + PartialEq {
+    pub fn contains_value(&self, expected_value: &T) {
+        match self.subject {
+            &Some(ref val) => {
+                if !val.eq(expected_value) {
+                    panic!(build_failure_string(&format!("<{}>", expected_value), &format!("<{}>", val)));
+                }
+            },
+            &None => panic!(build_failure_string(&format!("<{}>", expected_value), "empty"))
+        };
+
+        fn build_failure_string(containing: &str, actual: &str) -> String {
+            format!("expected option containing {} but was {}", containing, actual)
+        }
+    }
+
+    pub fn is_some(&self) {
+        match self.subject {
+            &None => panic!(format!("expected non-empty option but was empty")),
+            &Some(_) => ()
+        };
+    }
+
+    pub fn is_none(&self) {
+        match self.subject {
+            &None => (),
+            &Some(ref val) => panic!(format!("expected empty option but contained <{}>", val))
+        };
+    }
+}
+
 impl<'s, S> Spec<'s, S> {
     pub fn map<F, T>(self, mapping_function: F) -> Spec<'s, T>
         where F: Fn(&'s S) -> &'s T {
--- tests/spectral.rs
+++ tests/spectral.rs
@@ -27,6 +27,52 @@ fn should_panic_if_vec_length_does_not_match_expected() {
 }
 
 #[test]
+fn should_not_panic_if_option_is_expected_to_contain_value_and_does() {
+    let option = Some("Hello");
+    Spec::assert_that(&option).is_some();
+}
+
+#[test]
+#[should_panic]
+fn should_panic_if_option_is_expected_to_contain_value_and_does_not() {
+    let option: Option<&str> = None;
+    Spec::assert_that(&option).is_some();
+}
+
+#[test]
+fn should_not_panic_if_option_contains_expected_value() {
+    let option = Some("Hello");
+    Spec::assert_that(&option).contains_value(&"Hello");
+}
+
+#[test]
+#[should_panic]
+fn should_panic_if_option_does_not_contain_expected_value() {
+    let option = Some("Hello");
+    Spec::assert_that(&option).contains_value(&"Hi");
+}
+
+#[test]
+#[should_panic]
+fn should_panic_if_option_is_none_but_expected_value() {
+    let option: Option<&str> = None;
+    Spec::assert_that(&option).contains_value(&"Hello");
+}
+
+#[test]
+fn should_not_panic_if_option_is_empty() {
+    let option: Option<&str> = None;
+    Spec::assert_that(&option).is_none();
+}
+
+#[test]
+#[should_panic]
+fn should_panic_if_option_is_not_empty_but_was_expected_as_empty() {
+    let option = Some("Hello");
+    Spec::assert_that(&option).is_none();
+}
+
+#[test]
 fn should_be_able_to_map_to_inner_field_of_struct_when_matching() {
     let test_struct = TestStruct { value: 5 };
     Spec::assert_that(&test_struct).map(|val| &val.value).is_equal_to(&5);
