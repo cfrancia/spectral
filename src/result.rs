eaa5e1d7a026cacfab71128cfe7d685e2c7c5bec
cfrancia <cfrancia@users.noreply.github.com>
2016-10-13 23:00:20 +1100

    is_ok() and is_error() will now return a Spec containing the unwrapped value

--- src/result.rs
+++ src/result.rs
@@ -2,12 +2,12 @@ use super::{AssertionFailure, Spec};
 
 use std::fmt::Debug;
 
-pub trait ResultAssertions<T, E>
+pub trait ResultAssertions<'s, T, E>
     where T: Debug,
           E: Debug
 {
-    fn is_ok(&mut self);
-    fn is_error(&mut self);
+    fn is_ok(&mut self) -> Spec<'s, T>;
+    fn is_error(&mut self) -> Spec<'s, E>;
 }
 
 pub trait ContainingResultAssertions<T, E>
@@ -77,40 +77,58 @@ fn build_detail_message<T: Debug>(variant: &'static str, value: T) -> String {
     format!("Result[{}] containing <{:?}>", variant, value)
 }
 
-impl<'s, T, E> ResultAssertions<T, E> for Spec<'s, Result<T, E>>
+impl<'s, T, E> ResultAssertions<'s, T, E> for Spec<'s, Result<T, E>>
     where T: Debug,
           E: Debug
 {
     /// Asserts that the subject is `Ok`. The value type must be a `Result`.
     ///
+    /// This will return a new `Spec` containing the unwrapped value if it is `Ok`.
+    ///
     /// ```rust,ignore
     /// assert_that(&Result::Ok::<usize, usize>(1)).is_ok();
     /// ```
-    fn is_ok(&mut self) {
-        match self.subject {
-            &Ok(_) => (),
+    fn is_ok(&mut self) -> Spec<'s, T> {
+        return match self.subject {
+            &Ok(ref val) => {
+                Spec {
+                    subject: val,
+                    description: self.description,
+                }
+            }
             &Err(ref err) => {
                 AssertionFailure::from_spec(self)
                     .with_expected(format!("result[ok]"))
                     .with_actual(format!("result[error]<{:?}>", err))
                     .fail();
+
+                unreachable!();
             }
         };
     }
 
     /// Asserts that the subject is `Err`. The value type must be a `Result`.
     ///
+    /// This will return a new `Spec` containing the unwrapped value if it is `Err`.
+    ///
     /// ```rust,ignore
     /// assert_that(&Result::Err::<usize, usize>(1)).is_error();
     /// ```
-    fn is_error(&mut self) {
-        match self.subject {
-            &Err(_) => (),
+    fn is_error(&mut self) -> Spec<'s, E> {
+        return match self.subject {
+            &Err(ref val) => {
+                Spec {
+                    subject: val,
+                    description: self.description,
+                }
+            }
             &Ok(ref val) => {
                 AssertionFailure::from_spec(self)
                     .with_expected(format!("result[error]"))
                     .with_actual(format!("result[ok]<{:?}>", val))
                     .fail();
+
+                unreachable!();
             }
         };
     }
@@ -135,6 +153,12 @@ mod tests {
     }
 
     #[test]
+    fn should_return_unwrapped_value_if_subject_is_ok() {
+        let result: Result<&str, &str> = Ok("Hello");
+        assert_that(&result).is_ok().is_equal_to(&"Hello");
+    }
+
+    #[test]
     fn should_not_panic_if_result_is_expected_to_be_error_and_is() {
         let result: Result<&str, &str> = Err("Oh no");
         assert_that(&result).is_error();
@@ -145,6 +169,12 @@ mod tests {
     fn should_panic_if_result_is_expected_to_be_error_and_is_not() {
         let result: Result<&str, &str> = Ok("Hello");
         assert_that(&result).is_error();
+    }
+
+    #[test]
+    fn should_return_unwrapped_value_if_subject_is_err() {
+        let result: Result<&str, &str> = Err("Hello");
+        assert_that(&result).is_error().is_equal_to(&"Hello");
     }
 
     #[test]
